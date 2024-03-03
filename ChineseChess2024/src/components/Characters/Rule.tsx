c985f9a8bb856c59ed2e96764f13da3bcc078a5c
dinhnhatbang <dinhnhatbang@gmail.com>
2024-03-03 12:10:30 +0700

    feat: solider moving rule

--- ChineseChess2024/src/components/Characters/Rule.tsx
+++ ChineseChess2024/src/components/Characters/Rule.tsx
@@ -1,4 +1,5 @@
 import {CharacterEnum, CharacterTypeEnum} from '../../enum/character';
+import {Color} from '../../enum/color';
 import {MovingCharacterType} from '../../types/characterType';
 
 const eatingSameColor = (
@@ -11,17 +12,61 @@ const eatingSameColor = (
   );
 };
 
-const soliderRule = (
+const soliderCanNotMoveRule = (
   previous: MovingCharacterType,
   current: MovingCharacterType,
 ) => {
+  const isBackward = () => {
+    if (previous.character.color === Color.Red) {
+      if (previous.row.charCodeAt(0) > current.row.charCodeAt(0)) {
+        return true;
+      }
+    }
+    if (previous.character.color === Color.Black) {
+      if (previous.row.charCodeAt(0) < current.row.charCodeAt(0)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  const isCrossRiver = () => {
+    console.log('previous', previous.row.charCodeAt(0));
+    if (
+      previous.character.color === Color.Red &&
+      previous.row.charCodeAt(0) > 69
+    ) {
+      return true;
+    }
+    if (
+      previous.character.color === Color.Black &&
+      previous.row.charCodeAt(0) < 70
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  if (isBackward()) {
+    return true;
+  }
   const rowDiff = Math.abs(
     previous.row.charCodeAt(0) - current.row.charCodeAt(0),
   );
+
   const columnDiff = Math.abs(previous.column - current.column);
   if (rowDiff + columnDiff > 1) {
     return true;
   }
+
+  console.log('isCrossRiver', isCrossRiver(), rowDiff);
+
+  if (!isCrossRiver() && columnDiff == 1) {
+    return true;
+  }
+
   return false;
 };
 
@@ -33,7 +78,7 @@ export const canNotMove = (
     return true;
   }
   if (previous.character.name === CharacterEnum.Solider) {
-    return soliderRule(previous, current);
+    return soliderCanNotMoveRule(previous, current);
   }
 
   return false;
